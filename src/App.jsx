f79e3a4b95c37229b7cb4da3917087f05b442810
kacperlechicki <klechicki@poczta.fm>
2023-12-19 08:53:43 +0100

    basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 // import React from 'react';
+import styles from './styles';
 
 const App = () => (
 	<div className='bg-primary w-full overflow-hidden'>
-		<h1>Hello World</h1>
+		<div className={`${styles.paddingX} ${styles.flexCenter}`}>
+			<div className={`${styles.boxWidth}`}>NAVBAR</div>
+		</div>
+
+		<div className={`${styles.flexStart} bg-primary`}>
+			<div className={`${styles.boxWidth}`}>HERO</div>
+		</div>
+
+		<div className={`${styles.flexStart} ${styles.paddingX} bg-primary`}>
+			<div className={`${styles.boxWidth}`}>STATS</div>
+		</div>
 	</div>
 );
 
